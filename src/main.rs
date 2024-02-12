0340acf44d0216708ac1d1b4b07ed75ed7df9a06
fabioseixas <dev.fabioseixas@gmail.com>
2024-02-12 06:21:59 -0300

    feat: wip - create transaction

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use axum::{
-    http::StatusCode,
     debug_handler,
-    extract::{State},
+    extract::{Path, State},
+    http::StatusCode,
     response::{IntoResponse, Json},
-    routing::get,
+    routing::{get, post},
     Router,
 };
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use sqlx::{
     postgres::{PgPoolOptions, PgRow},
     Postgres, Row,
 };
+
+use types::CreateTransactionPayload;
+
+mod types;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Cliente {
@@ -34,7 +39,11 @@ struct Saldo {
 
 impl Saldo {
     fn new(id: i32, cliente_id: i32, valor: i32) -> Self {
-        Self { id, cliente_id, valor }
+        Self {
+            id,
+            cliente_id,
+            valor,
+        }
     }
 }
 
@@ -60,6 +69,54 @@ async fn get_saldos(State(state): State<sqlx::pool::Pool<Postgres>>) -> impl Int
     Json(serde_json::to_value(&saldos).unwrap())
 }
 
+#[debug_handler]
+async fn transaction(
+    State(state): State<sqlx::pool::Pool<Postgres>>,
+    Path(client_id): Path<String>,
+    Json(payload): Json<CreateTransactionPayload>,
+) -> impl IntoResponse {
+    println!("{:?}", payload);
+    println!("{:?}", client_id);
+    match sqlx::query("SELECT * from clientes WHERE id = $1")
+        .bind(client_id.parse::<i32>().unwrap())
+        .map(|row: PgRow| Cliente::new(row.get(0), row.get(1), row.get(2)))
+        .fetch_one(&state)
+        .await
+    {
+        Ok(cliente) => {
+            println!("OK!");
+            sqlx::query(
+                "INSERT INTO transacoes 
+                 (cliente_id, valor, tipo, descricao)
+                 VALUES ($1, $2, $3, $4)",
+            )
+            .bind(client_id.parse::<i32>().unwrap())
+            .bind(payload.valor)
+            .bind(payload.tipo)
+            .bind(payload.descricao)
+            .execute(&state)
+            .await
+            .expect("error while insert transacoes");
+
+            (
+                StatusCode::CREATED,
+                Json(serde_json::to_value(cliente).unwrap()),
+            )
+        }
+        Err(_) => {
+            println!("ERROR!");
+            (
+                StatusCode::BAD_REQUEST,
+                Json(json!({
+                    "message":"cliente inexistente"
+                })),
+            )
+        }
+    }
+
+    // Json(serde_json::to_value(&saldos).unwrap())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let pool = PgPoolOptions::new()
@@ -70,6 +127,7 @@ async fn main() -> Result<(), sqlx::Error> {
     let app = Router::new()
         .route("/clientes", get(get_clientes))
         .route("/saldos", get(get_saldos))
+        .route("/clientes/:id/transacoes", post(transaction))
         .with_state(pool.clone());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
